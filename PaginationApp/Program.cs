c75e7d37e5d6181ec8fdeea232ca566b23279af1
Marcos Aidos <30325304+AidosMarcos@users.noreply.github.com>
2018-10-28 16:59:13 +0000

    Adicionar arquivos de projeto.

---
+++ PaginationApp/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        /// <summary>
+        ///     Criar um metodo que gera paginação para um footer
+        /// </summary>
+        /// <param name="currentPage">Pagina atual a ser exibida</param>
+        /// <param name="totalPages">Numero total de paginas</param>
+        /// <param name="boundaries">Quantidade de paginas a mostrar no inicio e fim</param>
+        /// <param name="around">Quantidade de paginas a mostrar a volta da currentPage</param>
+        public static string Paginaçao(int currentPage, int totalPages, int boundaries, int around)
+        {
+            string resultado = "";
+
+            var paginas = new List<int>();
+
+            //verificar se a currentPage é maior que 0 e menor ou igual que totalPages
+            if (currentPage > 0 && currentPage <= totalPages || boundaries < 0 && around < 0)
+            {
+                paginas.Add(currentPage);
+            }
+            else
+            {
+                return "Dados inseridos não podem ser usados.";
+            }
+
+            //Adicionar as boundaries iniciais
+            for (int i = 1; i <= boundaries && i < currentPage; i++)
+            {
+                //adicionar i se numero i ainda não existir na lista
+                if (!paginas.Contains(i))
+                {
+                    paginas.Add(i);
+                }
+            }
+
+            //Adicionar as boundaries finais
+            for (int i = totalPages; i > (totalPages - boundaries) && i > currentPage; i--)
+            {
+                //adicionar i se numero i ainda não existir na lista
+                if (!paginas.Contains(i))
+                {
+                    paginas.Add(i);
+                }
+            }
+
+            //Adicionar os around antes de currentPage
+            for (int i = currentPage - around; i < currentPage; i++)
+            {
+                //adicionar i se ainda existir na lista e verificar que i é maior que 1 caso o valor de (currentPage - around) seja menor que 0
+                if (!paginas.Contains(i) && i >= 1)
+                {
+                    paginas.Add(i);
+                }
+            }
+
+            //Adicionar os around depois de currentPage
+            for (int i = currentPage + around; i > currentPage; i--)
+            {
+                //adicionar i se ainda existir na lista e verificar que i é menor que totalPages caso o valor de (currentPage + around) seja maior que totalPages
+                if (!paginas.Contains(i) && i <= totalPages)
+                {
+                    paginas.Add(i);
+                }
+            }
+
+            //Ordenar a lista por ordem crescente
+            paginas = paginas.OrderBy(item => item).ToList();
+
+            var paginaAnterior = 1;
+           
+            //Passar a string correctamente para a variavel e adicionar as ...
+            foreach (var pagina in paginas)
+            {
+                //Se a diferença entra a pagina atual e a anterior for maior que 1 significa que exite uma falha na sequencia de numeros então adiciona ...
+                if (pagina - paginaAnterior > 1)
+                {
+                    resultado += "... ";
+                }
+                paginaAnterior = pagina;
+
+                //Adicionar espaços entre os numeros, menos no ultimo, para facilitar leitura do output
+                if (pagina == paginas.Last())
+                {
+                    resultado += pagina;
+                    //Em case de ainda haver numeros até totalPages, visto não os querermos mostrar adicionamos ...
+                    if (totalPages - pagina > 1)
+                    {
+                        resultado += " ...";
+                    }
+                }
+                else
+                {
+                    resultado += pagina + " ";
+                }
+            }
+
+            return resultado;
+        }
+
+    }
+
+}
---
+++ UnitTestPaginationApp/UnitTest1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaginationApp;
+
+namespace UnitTestPaginationApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodValoresSaoZero()
+        {
+
+            int currentPage = 0;
+            int totalPages = 0;
+            int boundaries = 0;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "Dados inseridos não podem ser usados.";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresBoundariesAroundMaiorQueTotalPages()
+        {
+
+            int currentPage = 1;
+            int totalPages = 1;
+            int boundaries = 2;
+            int around = 2;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "1";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresAroundZero()
+        {
+
+            int currentPage = 5;
+            int totalPages = 10;
+            int boundaries = 3;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "1 2 3 ... 5 ... 8 9 10";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresCurrentPageMaiorQueTotalPages()
+        {
+
+            int currentPage = 10;
+            int totalPages = 1;
+            int boundaries = 0;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "Dados inseridos não podem ser usados.";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresBoundariesAroundMaioresQueTotalPages()
+        {
+            int currentPage = 5;
+            int totalPages = 10;
+            int boundaries = 20;
+            int around = 20;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "1 2 3 4 5 6 7 8 9 10";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresCurrentPageIgualTotalPages()
+        {
+
+            int currentPage = 10;
+            int totalPages = 10;
+            int boundaries = 3;
+            int around = 2;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "1 2 3 ... 8 9 10";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresCurrentPageIgualTotalPagesComZeroBoundaries()
+        {
+
+            int currentPage = 10;
+            int totalPages = 10;
+            int boundaries = 0;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "... 10";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresCurrentPageIgualTotalPagesComAroundMasSemBoundaries()
+        {
+
+            int currentPage = 10;
+            int totalPages = 10;
+            int boundaries = 0;
+            int around = 4;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "... 6 7 8 9 10";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresCurrentPageIgualTotalPagesComAroundMasSemBoundaries2()
+        {
+
+            int currentPage = 1;
+            int totalPages = 10;
+            int boundaries = 0;
+            int around = 4;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "1 2 3 4 5 ...";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresBoundariesCincoAroundZero()
+        {
+
+            int currentPage = 8;
+            int totalPages = 20;
+            int boundaries = 5;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperado = "1 2 3 4 5 ... 8 ... 16 17 18 19 20";
+
+            Assert.AreEqual(resultadoEsperadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresBoundariesEAroundZeroCurrentPageCincoETotalPagesVinte()
+        {
+
+            int currentPage = 5;
+            int totalPages = 20;
+            int boundaries = 0;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperado = "... 5 ...";
+
+            Assert.AreEqual(resultadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresGrandesBoundariesAroundDez()
+        {
+
+            int currentPage = 10000;
+            int totalPages = 40000;
+            int boundaries = 10;
+            int around = 10;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperado = "1 2 3 4 5 6 7 8 9 10 ... 9990 9991 9992 9993 9994 9995 9996 9997 9998 9999 10000 10001 10002" +
+                " 10003 10004 10005 10006 10007 10008 10009 10010 ... 39991 39992 39993 39994 39995 39996 39997 39998 39999 40000";
+
+            Assert.AreEqual(resultadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresCurrentPageMaxValue()
+        {
+
+            int currentPage = 100000;
+            int totalPages = int.MaxValue;
+            int boundaries = 1;
+            int around = 0;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperado = "1 ... 100000 ... 2147483647";
+
+            Assert.AreEqual(resultadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresBoundariesAroundNegativos()
+        {
+
+            int currentPage = 8;
+            int totalPages = 20;
+            int boundaries = -1;
+            int around = -2;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperado = "... 8 ...";
+
+            Assert.AreEqual(resultadoEsperado, metodo);
+        }
+
+        [TestMethod]
+        public void TestMethodValoresTotalPagesNegativo()
+        {
+
+            int currentPage = 4;
+            int totalPages = -2;
+            int boundaries = 3;
+            int around = 3;
+
+            string metodo = Program.Paginaçao(currentPage, totalPages, boundaries, around);
+
+            string resultadoEsperadoEsperadoEsperadoo = "Dados inseridos não podem ser usados.";
+
+            Assert.AreEqual(resultadoEsperadoEsperadoEsperadoo, metodo);
+        }
+    }
+}
